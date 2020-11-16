2d0657dd4a397c06ccefce7591cabd305b763dc2
Milkky <Milkky598@yandex.ru>
2020-11-16 14:02:10 +0500

    change yml to config

--- src/main/java/ru/mobydrake/testTask/repository/BookDaoImpl.java
+++ src/main/java/ru/mobydrake/testTask/repository/BookDaoImpl.java
@@ -6,40 +6,32 @@ import org.springframework.stereotype.Repository;
 import ru.mobydrake.testTask.entity.Book;
 import ru.mobydrake.testTask.mapper.BookMapper;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
 public class BookDaoImpl implements BookDao{
 
-    private final DataSource dataSource;
+    private final JdbcTemplate jdbcTemplate;
     private final BookMapper mapper;
-
-    private JdbcTemplate jdbcTemplate;
-
-    private final String SQL_WHERE_ID = "SELECT * FROM Book WHERE id = ?";
-    private final String SQL_SELECT_ALL = "SELECT * FROM Book";
-    private final String SQL_CREATE_BOOK = "INSERT INTO Book (title, author, description) VALUES (?, ?, ?)";
-
-    @PostConstruct
-    private void init() {
-        jdbcTemplate = new JdbcTemplate(dataSource);
-    }
 
     @Override
     public Book findBookById(Long id) {
+        final String SQL_WHERE_ID = "SELECT * FROM Book WHERE id = ?";
         return jdbcTemplate.queryForObject(SQL_WHERE_ID, mapper, id);
     }
 
     @Override
     public List<Book> findAllBook() {
+        final String SQL_SELECT_ALL = "SELECT * FROM Book";
         return jdbcTemplate.query(SQL_SELECT_ALL, mapper);
     }
 
     @Override
     public int save(Book book) {
+        if (book == null || book.getTitle() == null || book.getAuthor() == null) return 0;
+
+        final String SQL_CREATE_BOOK = "INSERT INTO Book (title, author, description) VALUES (?, ?, ?)";
         return jdbcTemplate.update(SQL_CREATE_BOOK, book.getTitle(), book.getAuthor(), book.getDescription());
     }
 }
